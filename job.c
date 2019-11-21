11c431ecf368b769d16385e137c09a5bdb3d0e11
Michael R Sweet <michael.r.sweet@gmail.com>
2019-11-20 22:17:29 -0500

    Add initial files.

---
+++ job.c
@@ -0,0 +1,16 @@
+//
+// ??? for LPrint, a Label Printer Utility
+//
+// Copyright © 2019 by Michael R Sweet.
+//
+// Licensed under Apache License v2.0.  See the file "LICENSE" for more
+// information.
+//
+
+//
+// Include necessary headers...
+//
+
+#include "lprint.h"
+
+
---
+++ lprint.h
@@ -0,0 +1,22 @@
+//
+// Header file for LPrint, a Label Printer Utility
+//
+// Copyright © 2019 by Michael R Sweet.
+//
+// Licensed under Apache License v2.0.  See the file "LICENSE" for more
+// information.
+//
+
+#ifndef _LPRINT_H_
+#  define _LPRINT_H_
+
+//
+// Include necessary headers...
+//
+
+#  include <cups/cups.h>
+#  include <cups/raster.h>
+#  include "config.h"
+
+
+#endif /* !_LPRINT_H_ */
