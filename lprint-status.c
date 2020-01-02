bab638095c7165f02071b0f6fc8e9e9d7b1ae337
Michael R Sweet <michael.r.sweet@gmail.com>
2020-01-02 16:46:51 -0500

    Fix lprint status subcommand support for -d PRINTER.
    If no printer specified, status of server is shown.

--- lprint-status.c
+++ lprint-status.c
@@ -24,26 +24,33 @@ lprintDoStatus(
     cups_option_t *options)		// I - Options
 {
   http_t		*http;		// HTTP connection
+  const char		*printer_name;	// Printer name
+  char			resource[1024];	// Resource path
   ipp_t			*request,	// IPP request
 			*response;	// IPP response
-  ipp_attribute_t	*attr;		// Current attribute
   int			i,		// Looping var
 			count,		// Number of reasons
-			state;		// system-state value
-  time_t		state_time;	// system-state-change-time value
-  const char		*reason;	// system-state-reasons value
-  static const char * const states[] =	// system-state values
+			state;		// *-state value
+  ipp_attribute_t	*state_reasons;	// *-state-reasons attribute
+  time_t		state_time;	// *-state-change-time value
+  const char		*reason;	// *-state-reasons value
+  static const char * const states[] =	// *-state strings
   {
     "idle",
     "processing jobs",
     "stopped"
   };
-  static const char * const requested[] =
-  {					// Requested attributes
+  static const char * const pattrs[] =
+  {					// Requested printer attributes
+    "printer-state",
+    "printer-state-change-date-time",
+    "printer-state-reasons"
+  };
+  static const char * const sysattrs[] =
+  {					// Requested system attributes
     "system-state",
     "system-state-change-date-time",
-    "system-state-reasons",
-    "system-up-time"
+    "system-state-reasons"
   };
 
 
@@ -54,31 +61,49 @@ lprintDoStatus(
     return (0);
   }
 
-  request = ippNewRequest(IPP_OP_GET_SYSTEM_ATTRIBUTES);
-  ippAddString(request, IPP_TAG_OPERATION, IPP_TAG_URI, "system-uri", NULL, "ipp://localhost/ipp/system");
-  ippAddString(request, IPP_TAG_OPERATION, IPP_TAG_NAME, "requesting-user-name", NULL, cupsUser());
-  ippAddStrings(request, IPP_TAG_OPERATION, IPP_TAG_KEYWORD, "requested-attributes", (int)(sizeof(requested) / sizeof(requested[0])), NULL, requested);
+  if ((printer_name = cupsGetOption("printer-name", num_options, options)) != NULL)
+  {
+    request = ippNewRequest(IPP_OP_GET_PRINTER_ATTRIBUTES);
+    lprintAddPrinterURI(request, printer_name, resource, sizeof(resource));
+    ippAddString(request, IPP_TAG_OPERATION, IPP_TAG_NAME, "requesting-user-name", NULL, cupsUser());
+    ippAddStrings(request, IPP_TAG_OPERATION, IPP_TAG_KEYWORD, "requested-attributes", (int)(sizeof(pattrs) / sizeof(pattrs[0])), NULL, pattrs);
 
-  response = cupsDoRequest(http, request, "/ipp/system");
+    response      = cupsDoRequest(http, request, resource);
+    state         = ippGetInteger(ippFindAttribute(response, "printer-state", IPP_TAG_ENUM), 0);
+    state_time    = ippDateToTime(ippGetDate(ippFindAttribute(response, "printer-state-change-date-time", IPP_TAG_DATE), 0));
+    state_reasons = ippFindAttribute(response, "printer-state-reasons", IPP_TAG_KEYWORD);
+  }
+  else
+  {
+    request = ippNewRequest(IPP_OP_GET_SYSTEM_ATTRIBUTES);
+    ippAddString(request, IPP_TAG_OPERATION, IPP_TAG_URI, "system-uri", NULL, "ipp://localhost/ipp/system");
+    ippAddString(request, IPP_TAG_OPERATION, IPP_TAG_NAME, "requesting-user-name", NULL, cupsUser());
+    ippAddStrings(request, IPP_TAG_OPERATION, IPP_TAG_KEYWORD, "requested-attributes", (int)(sizeof(sysattrs) / sizeof(sysattrs[0])), NULL, sysattrs);
 
-  if ((state = ippGetInteger(ippFindAttribute(response, "system-state", IPP_TAG_ENUM), 0)) < IPP_PSTATE_IDLE)
+    response      = cupsDoRequest(http, request, "/ipp/system");
+    state         = ippGetInteger(ippFindAttribute(response, "system-state", IPP_TAG_ENUM), 0);
+    state_time    = ippDateToTime(ippGetDate(ippFindAttribute(response, "system-state-change-date-time", IPP_TAG_DATE), 0));
+    state_reasons = ippFindAttribute(response, "system-state-reasons", IPP_TAG_KEYWORD);
+  }
+
+  if (state < IPP_PSTATE_IDLE)
     state = IPP_PSTATE_IDLE;
   else if (state > IPP_PSTATE_STOPPED)
     state = IPP_PSTATE_STOPPED;
 
-  state_time = ippDateToTime(ippGetDate(ippFindAttribute(response, "system-state-change-date-time", IPP_TAG_DATE), 0));
-
   printf("Running, %s since %s\n", states[state - IPP_PSTATE_IDLE], httpGetDateString(state_time));
 
-  if ((attr = ippFindAttribute(response, "system-state-reasons", IPP_TAG_KEYWORD)) != NULL)
+  if (state_reasons)
   {
-    for (i = 0, count = ippGetCount(attr); i < count; i ++)
+    for (i = 0, count = ippGetCount(state_reasons); i < count; i ++)
     {
-      reason = ippGetString(attr, i, NULL);
+      reason = ippGetString(state_reasons, i, NULL);
       if (strcmp(reason, "none"))
         puts(reason);
     }
   }
+
+  ippDelete(response);
 
   return (0);
 }
