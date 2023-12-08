d6508afa0c551aa3b7f3518f36ea40d4130bcafa
Michael R Sweet <msweet@msweet.org>
2023-12-08 12:28:07 -0500

    Update model lists for ZPL and TSPL printers.

--- lprint-tspl.h
+++ lprint-tspl.h
@@ -9,5 +9,5 @@
 
 { "tspl_203dpi",		"203dpi TSPL Label Printer", NULL, NULL },
 { "tspl_rollo-x1038_203dpi",	"Rollo X1038",
-				"MFG:Printer;MDL:Thermal Printer;", NULL },
+  "MFG:Printer;MDL:Thermal Printer;CMD:XPP,XL;", NULL },
 { "tspl_300dpi",		"300dpi TSPL Label Printer", NULL, NULL },
